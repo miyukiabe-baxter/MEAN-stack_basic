1ed2b0670d007443a22d9c899acd7994e7380452
Miyuki Abe <miyuki0809@gmail.com>
2019-12-01 18:09:42 -0500

    completed upto Emit Binding 2-24

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MEAN-stack';
+  storedPosts = []
+
+  onPostAdded(post) {
+    this.storedPosts.push(post)
+  }
 }
---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,19 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+
+export class PostCreateComponent {
+  enteredTitle = '';
+  enteredValue = '';
+  @Output() postCreated = new EventEmitter();
+
+  onAddPost() {
+    const post = { title: this.enteredTitle, content: this.enteredValue };
+    this.postCreated.emit(post)
+  }
+}
